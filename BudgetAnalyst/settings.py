0fcb80e4f411acc1a9cab47db4cc1dcfa43d0782
Adrian Merle <adrian.merle@insa-lyon.fr>
2024-05-05 19:51:11 +0100

    Fixed logo not showing

--- BudgetAnalyst/settings.py
+++ BudgetAnalyst/settings.py
@@ -120,9 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATIC_URL = 'App/static/'
+STATICFILES_DIRS = [BASE_DIR / STATIC_URL]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
